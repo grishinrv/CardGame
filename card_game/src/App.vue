1be47516dd5d1d25b77fe55ffd2f57f6b225a5cd
grishinrv <roman1988grishin@gmail.com>
2020-03-28 21:15:14 +0300

    Home task

---
+++ card_game/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="gameContainer">
+      <Card />
+      <Card />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#gameContainer{
+  display: flex;
+  justify-content: space-around;
+}
+</style>
